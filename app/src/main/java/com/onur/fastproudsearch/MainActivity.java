be1793d272d46d52391752c283bf884487ee7f82
Mert Ramazan <86368940+MertRamazanEMEK@users.noreply.github.com>
2024-05-14 13:56:12 +0300

    settings_activity
    settings_activity

--- app/src/main/java/com/onur/fastproudsearch/MainActivity.java
+++ app/src/main/java/com/onur/fastproudsearch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.onur.fastproudsearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,7 +40,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Ayarlar butonuna tıklandığında yapılacak işlemler buraya yazılır
                 Toast.makeText(MainActivity.this, "Ayarlar butonuna tıklandı", Toast.LENGTH_SHORT).show();
-
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent); // MainActivity'e geçiş yap
+                finish(); // LoginActivity'i kapat
             }
         });
 
--- app/src/main/java/com/onur/fastproudsearch/SettingsActivity.java
+++ app/src/main/java/com/onur/fastproudsearch/SettingsActivity.java
@@ -1,4 +1,53 @@
 package com.onur.fastproudsearch;
 
-public class SettingsActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private SwitchCompat themeSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        themeSwitch = findViewById(R.id.themeSwitch);
+
+        themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Null kontrolü yap
+            if (buttonView == null) {
+                return;
+            }
+
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+
+            SharedPreferences.Editor editor = getSharedPreferences("settings", MODE_PRIVATE).edit();
+            editor.putBoolean("darkTheme", isChecked);
+            editor.apply();
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getSharedPreferences("settings", MODE_PRIVATE);
+        boolean darkThemeEnabled = prefs.getBoolean("darkTheme", false);
+        themeSwitch.setChecked(darkThemeEnabled);
+
+        if (darkThemeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
 }
