57b80baff6b036a5ad40af425dcb7c6f5951fd39
Mert Ramazan <86368940+MertRamazanEMEK@users.noreply.github.com>
2024-05-14 14:31:44 +0300

    fix
    fix

--- app/src/main/java/com/onur/fastproudsearch/SignupActivity.java
+++ app/src/main/java/com/onur/fastproudsearch/SignupActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputLayout;
 import java.util.HashMap;
