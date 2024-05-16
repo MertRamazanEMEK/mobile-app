565f69323952c05718b18c499c9b3fca68e3b80a
B1ack-D3ath <128817169+B1ack-D3ath@users.noreply.github.com>
2024-05-16 23:11:24 +0300

    Fix Error

--- app/src/main/java/com/onur/fastproudsearch/MainActivity.java
+++ app/src/main/java/com/onur/fastproudsearch/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.textfield.TextInputLayout;
