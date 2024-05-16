c3fd77b7dbc5e06c5ffd818fbbc8184a26709ae8
Mert Ramazan <86368940+MertRamazanEMEK@users.noreply.github.com>
2024-05-16 22:10:58 +0300

    added_fix
    fix

--- app/src/main/java/com/onur/fastproudsearch/LoginActivity.java
+++ app/src/main/java/com/onur/fastproudsearch/LoginActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameEditText, passwordEditText;
-    private Button loginButton,signupButton;
+    private Button loginButton, signupButton, forgotPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,41 +40,41 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initComponents(){
-        // XML dosyasındaki bileşenleri Java değişkenlerine atama
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         loginButton = findViewById(R.id.loginButton);
         signupButton = findViewById(R.id.signupButton);
+        forgotPasswordButton = findViewById(R.id.forgotpasswordButton);
     }
 
     private void registerEventHandler(){
-        // Giriş butonuna tıklama işlemi
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Kullanıcı adı ve şifre kontrolü
                 String username = usernameEditText.getText().toString().trim();
                 String password = passwordEditText.getText().toString().trim();
 
-                // Kullanıcı adı ve şifre boş değilse devam et
                 if (!username.isEmpty() && !password.isEmpty()) {
-                    // MainActivity'e geçiş yapmak için Intent oluştur
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent); // MainActivity'e geçiş yap
+                    startActivity(intent);
                 } else {
-                    // Kullanıcıya uygun bir mesaj göster
                     Toast.makeText(LoginActivity.this, "Lütfen kullanıcı adı ve şifre girin", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        // Kaydol butonuna tıklama işlemi
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // SignupActivity'e geçiş yapmak için Intent oluştur
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent); // SignupActivity'e geçiş yap
+                startActivity(intent);
+            }
+        });
+
+        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
             }
         });
     }
