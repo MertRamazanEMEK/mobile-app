17f3b94e285f9fcac0be2490e42be339269acb06
B1ack-D3ath <128817169+B1ack-D3ath@users.noreply.github.com>
2024-05-20 12:55:11 +0300

    Fix error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:21.1.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 
 
 }
